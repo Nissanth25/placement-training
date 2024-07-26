1f42debac14af7eee00f35c8e6f7084d890364eb
Nissanth <147823208+Nissanth25@users.noreply.github.com>
2024-07-26 17:24:30 +0530

    Create Find the GCD of Two Numbers.c

---
+++ 26.07.2024/Find the GCD of Two Numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int main() {
+    int a = 48, b = 18;
+    printf("GCD: %d\n", gcd(a, b));
+    return 0;
+}

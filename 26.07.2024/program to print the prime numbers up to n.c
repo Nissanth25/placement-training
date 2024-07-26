f784a38147d75e984c8ebbce58bcb64481595285
Nissanth <147823208+Nissanth25@users.noreply.github.com>
2024-07-26 17:18:14 +0530

    Create program to print the prime numbers up to n.c

---
+++ 26.07.2024/program to print the prime numbers up to n.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int a,i,j;
+	printf("n: ");
+	scanf("%d",&a);
+			for(i=1;i<=a;i++)
+				{
+						for(j=2;j<=i;j++)
+							{
+							if(i%j==0)
+								break;
+				}
+	if(i==j)
+	{
+		printf("%d ",i);
+	}
+				}
+}

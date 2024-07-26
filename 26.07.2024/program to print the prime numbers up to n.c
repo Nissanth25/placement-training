ca58b9e7b3cd3ade206afccdee2237aee313cd60
Nissanth <147823208+Nissanth25@users.noreply.github.com>
2024-07-26 19:34:49 +0530

    Update program to print the prime numbers up to n.c

--- 26.07.2024/program to print the prime numbers up to n.c
+++ 26.07.2024/program to print the prime numbers up to n.c
@@ -4,16 +4,16 @@ int main()
 	int a,i,j;
 	printf("n: ");
 	scanf("%d",&a);
-			for(i=1;i<=a;i++)
-				{
-						for(j=2;j<=i;j++)
-							{
-							if(i%j==0)
-								break;
-				}
+	    for(i=1;i<=a;i++)
+		{
+	        for(j=2;j<=i;j++)
+		        {
+			if(i%j==0)
+			break;
+		        }
 	if(i==j)
 	{
 		printf("%d ",i);
 	}
-				}
+	        }
 }

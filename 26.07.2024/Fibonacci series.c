871a3556a12f37885de29bfd4f546bcd73b537d1
Nissanth <147823208+Nissanth25@users.noreply.github.com>
2024-07-26 17:18:54 +0530

    Create Fibonacci series.c

---
+++ 26.07.2024/Fibonacci series.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int n,a=0,b=1,c,i;
+	printf("n: ");
+	scanf("%d",&n);
+	printf("%d %d",a,b);
+	for(i=2;i<n;i++)
+		{
+			c=a+b;
+			printf(" %d",c);
+			a=b;
+			b=c;
+		}
+	printf(" ");
+	return 0;
+}

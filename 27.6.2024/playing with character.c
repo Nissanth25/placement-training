78cec0833b8bc7c9b8a6b99b5da45463f6f9f51c
Nissanth <147823208+Nissanth25@users.noreply.github.com>
2024-07-27 13:02:13 +0530

    playing with character.c

---
+++ 27.6.2024/playing with character.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() 
+{
+    char ch;
+    char s[100];
+    char sen[100];
+    scanf("%c", &ch);
+    scanf("%s", s);
+    scanf("\n");
+    scanf("%[^\n]s", &sen);
+    printf("%c\n", ch);
+    printf("%s\n", s);
+    printf("%s\n", sen);
+        
+    return 0;
+}

d11c397e6d55df6d5fead0e661e4aca3f7bd453f
Nissanth <147823208+Nissanth25@users.noreply.github.com>
2024-07-28 12:38:14 +0530

    arithmetic operators.py

---
+++ 28.07.2024/arithmetic operators.py
@@ -0,0 +1,10 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+if num2 != 0:
+    print("Division:", num1 / num2)
+else:
+    print("Division by zero is not allowed")

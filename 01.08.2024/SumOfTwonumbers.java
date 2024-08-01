d80cbc1b8c2236ed86cd6190b144ed72cbab955b
Nissanth <147823208+Nissanth25@users.noreply.github.com>
2024-08-01 19:31:44 +0530

    SumOfTwonumbers.java

---
+++ 01.08.2024/SumOfTwonumbers.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class SumOfTwoNumbers {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+        System.out.println("The sum of " + a + " and " + b + " is " + sum);
+    }
+}
+
+

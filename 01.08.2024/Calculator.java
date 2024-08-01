969ccd5b84d7b16325341331b97321709f08d128
Nissanth <147823208+Nissanth25@users.noreply.github.com>
2024-08-01 19:30:45 +0530

    Calculator.java

---
+++ 01.08.2024/Calculator.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+        int sum = num1 + num2;
+        System.out.println("The sum is: " + sum);
+        sc.close();
+    }
+}
+
+
+
+

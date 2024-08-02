edee4fab205780104bff7152341f1ed240d8ff22
Nissanth <147823208+Nissanth25@users.noreply.github.com>
2024-08-02 20:33:52 +0530

    Even or Odd.java

---
+++ 02.08.2024/Even or Odd.java
@@ -0,0 +1,9 @@
+public class EvenOdd {
+    public static void main(String[] args) {
+        int num = 7;
+        if(num % 2 == 0)
+            System.out.println(num + " is even.");
+        else
+            System.out.println(num + " is odd.");
+    }
+}

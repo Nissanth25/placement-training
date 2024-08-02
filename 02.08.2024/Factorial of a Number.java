ddc8731a2fb3d917d8b616e15fa42705af58a302
Nissanth <147823208+Nissanth25@users.noreply.github.com>
2024-08-02 20:34:35 +0530

    Factorial of a Number.java

---
+++ 02.08.2024/Factorial of a Number.java
@@ -0,0 +1,10 @@
+public class Factorial {
+    public static void main(String[] args) {
+        int num = 5;
+        int factorial = 1;
+        for(int i = 1; i <= num; ++i) {
+            factorial *= i;
+        }
+        System.out.println("Factorial of " + num + " is " + factorial);
+    }
+}

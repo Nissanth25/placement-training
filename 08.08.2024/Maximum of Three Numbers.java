ac21f43fb292fc81d63a16fbeb0ccc5ef3b7ff42
Nissanth <147823208+Nissanth25@users.noreply.github.com>
2024-08-08 13:09:36 +0530

    Maximum of Three Numbers.java

---
+++ 08.08.2024/Maximum of Three Numbers.java
@@ -0,0 +1,7 @@
+public class MaxOfThree {
+    public static void main(String[] args) {
+        int a = 10, b = 20, c = 15;
+        int max = Math.max(a, Math.max(b, c));
+        System.out.println("Maximum number is " + max);
+    }
+}

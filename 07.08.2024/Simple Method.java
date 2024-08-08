cc19d2fa99226df2c9a72379d5edcbdafd930aca
Nissanth <147823208+Nissanth25@users.noreply.github.com>
2024-08-08 13:03:00 +0530

    Simple Method.java

---
+++ 07.08.2024/Simple Method.java
@@ -0,0 +1,8 @@
+public class SimpleMethod {
+    public static void main(String[] args) {
+        greet("Alice");
+    }
+    public static void greet(String name) {
+        System.out.println("Hello, " + name + "!");
+    }
+}

bcb8e8e10ab17621e7ad0a6986a7a96c1afd9542
Nissanth <147823208+Nissanth25@users.noreply.github.com>
2024-08-08 13:08:57 +0530

    String Concatenation.java

---
+++ 08.08.2024/String Concatenation.java
@@ -0,0 +1,8 @@
+public class StringConcatenation {
+    public static void main(String[] args) {
+        String firstName = "John";
+        String lastName = "Doe";
+        String fullName = firstName + " " + lastName;
+        System.out.println("Full Name: " + fullName);
+    }
+}

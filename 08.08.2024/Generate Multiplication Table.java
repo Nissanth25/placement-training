322d85a68e5abbc3a45b68e67abbea3acf6fe748
Nissanth <147823208+Nissanth25@users.noreply.github.com>
2024-08-08 13:10:10 +0530

    Generate Multiplication Table.java

---
+++ 08.08.2024/Generate Multiplication Table.java
@@ -0,0 +1,8 @@
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        int num = 7;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " x " + i + " = " + (num * i));
+        }
+    }
+}
